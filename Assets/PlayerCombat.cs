8588c4cb4f58cb34cb388d0a147f1acd7fae8c8d
cbu-ben-taylor <benjamin.taylor@calbaptist.edu>
2021-09-28 01:05:21 -0700

    Added Knight sprite and attack animation (still needs sprites)

---
+++ Assets/PlayerCombat.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Attack();
+        }
+    }
+
+    void Attack() 
+    {
+        // Play attack animation
+        animator.SetTrigger("Attack");
+
+        // Detect enemies in range of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        // Damage them
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            // Do damage
+        }
+    }
+
+    void OnDrawGizmosSelected() 
+    {
+        if (attackPoint == null)
+        return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);    
+    }
+}
